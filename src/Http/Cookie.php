db76ab0442ac87b32a1a3d449fc5c9bd02bb5f8b
Falaleev Maxim <max@studio107.ru>
2016-08-26 11:43:29 +0300

    Refact cookie class

--- src/Http/Cookie.php
+++ src/Http/Cookie.php
@@ -2,82 +2,49 @@
 
 namespace Mindy\Http;
 
-/**
- * A Cookie instance stores a single cookie, including the cookie name, value, domain, path, expire, and secure.
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @package Mindy\Http
- * @since 1.0
- */
 class Cookie
 {
-    /**
-     * @var string name of the cookie
-     */
-    public $name;
-    /**
-     * @var string value of the cookie
-     */
-    public $value = '';
-    /**
-     * @var string domain of the cookie
-     */
-    public $domain = '';
-    /**
-     * @var integer the timestamp at which the cookie expires. This is the server timestamp. Defaults to 0, meaning "until the browser is closed".
-     */
-    public $expire = 0;
-    /**
-     * @var string the path on the server in which the cookie will be available on. The default is '/'.
-     */
-    public $path = '/';
-    /**
-     * @var boolean whether cookie should be sent via secure connection
-     */
-    public $secure = false;
-    /**
-     * @var boolean whether the cookie should be accessible only through the HTTP protocol.
-     * By setting this property to true, the cookie will not be accessible by scripting languages,
-     * such as JavaScript, which can effectly help to reduce identity theft through XSS attacks.
-     * Note, this property is only effective for PHP 5.2.0 or above.
-     */
-    public $httpOnly = false;
+    private $name;
+    private $value;
+    private $expires = 0;
+    private $maxAge = 0;
+    private $path;
+    private $domain;
+    private $secure = false;
+    private $httpOnly = false;
 
-    /**
-     * Constructor.
-     * @param string $name name of this cookie
-     * @param string $value value of this cookie
-     * @param array $options the configuration array consisting of name-value pairs
-     * that are used to configure this cookie
-     */
-    public function __construct($name, $value, $options = [])
+    public function __construct($name, $value = null, array $options = [])
     {
         $this->name = $name;
         $this->value = $value;
-
-        unset($options['name'], $options['value']);
-
-        $data = session_get_cookie_params();
-        $data['httpOnly'] = $data['httponly'];
-        $data['expire'] = $data['lifetime'];
-        unset($data['lifetime'], $data['httponly']);
-        
-        foreach (array_merge($data, $options) as $key => $value) {
+        foreach ($options as $key => $value) {
             $this->{$key} = $value;
         }
     }
 
-    /**
-     * Magic method to use the cookie object as a string without having to call value property first.
-     * <code>
-     * $value = (string)$cookies['name'];
-     * </code>
-     * Note, that you still have to check if the cookie exists.
-     * @return string The value of the cookie. If the value property is null an empty string will be returned.
-     * @since 1.1.11
-     */
     public function __toString()
     {
-        return (string)$this->value;
+        $cookieStringParts = [
+            urlencode($this->name) . '=' . urlencode($this->value),
+        ];
+        if ($this->domain) {
+            $cookieStringParts[] = sprintf("Domain=%s", $this->domain);
+        }
+        if ($this->path) {
+            $cookieStringParts[] = sprintf("Path=%s", $this->path);
+        }
+        if ($this->expires) {
+            $cookieStringParts[] = sprintf("Expires=%s", gmdate('D, d M Y H:i:s T', $this->expires));
+        }
+        if ($this->maxAge) {
+            $cookieStringParts[] = sprintf("Max-Age=%s", $this->maxAge);
+        }
+        if ($this->secure) {
+            $cookieStringParts[] = 'Secure';
+        }
+        if ($this->httpOnly) {
+            $cookieStringParts[] = 'HttpOnly';
+        }
+        return implode('; ', $cookieStringParts);
     }
 }
